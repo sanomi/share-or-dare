2648aa13b63fe3fb1eeab0d47d6b15964a9f6843
Jameela Huq <jameela.huq@gmail.com>
2015-10-10 02:58:40 -0700

    merging alice with huq

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,21 +1,27 @@
 angular.module('starter.controllers', [])
 
-.controller('homeCtrl', function($scope) {})
+  .controller('loginCtrl', function($scope, $http) {
 
-.controller('proposeCtrl', function($scope) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-})
+  })
 
-.controller('shareCtrl', function($scope) {
-  
-})
+  .controller('homeCtrl', function($scope, $http) {
+    var ref = "http://localhost:3000/truths";
+    $scope.truth = $http.get(ref);
+    var ref = "http://localhost:3000/dares";
+    $scope.dare = $http.get(ref);
+  })
 
-.controller('dareCtrl', function($scope) {
+  .controller('proposeCtrl', function($scope) {
+  })
 
-});
+  .controller('shareCtrl', function($scope, $http) {
+    var ref = "http://localhost:3000/truths";
+    $scope.truth = $http.get(ref);
+    console.log(truth);
+  })
+
+  .controller('dareCtrl', function($scope, $http) {
+    var ref = "http://localhost:3000/dares";
+    $scope.dare = $http.get(ref);
+    console.log($scope.dare);
+  });
